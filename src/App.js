35ca7bf1aade5352239e67f19b35d21d91e67a1d
payal agrawal <pagrawal312@gmail.com>
2017-03-16 13:46:00 -0700

    added fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      query:''
+    }
+  }
+
+  search(){
+    console.log("this.state", this.state);
+    const BASE_URL ='https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log(FETCH_URL);
+    fetch(FETCH_URL, {
+      method:'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json))
+    .catch(err => console.log('error', err))
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +31,14 @@ class App extends Component{
           <FormControl
             type="text"
             placeholder="search for an artist...."
+            value={this.state.query}
+            onChange={event => {this.setState({query:event.target.value})}}
+            onKeyPress={event => {
+              if(event.key=='Enter') {this.search()}
+            }
+            }
           />
-          <InputGroup.Addon>
+          <InputGroup.Addon onClick={()=>this.search()}>
             <Glyphicon glyph="search"></Glyphicon>
           </InputGroup.Addon>
         </InputGroup>
